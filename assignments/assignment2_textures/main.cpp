e47d6032246a76da4f752a64f947ba54a5af4b11
caleb.secrest <caleb.secrest@mymail.champlain.edu>
2024-09-24 23:46:20 -0400

    Textures & Character
    Added in some pictures to use as textures and for my character sprite.

--- assignments/assignment2_textures/main.cpp
+++ assignments/assignment2_textures/main.cpp
@@ -82,7 +82,7 @@ int main() {
     int width, height, nrChannels;
     stbi_set_flip_vertically_on_load(true);
 
-    unsigned char *data = stbi_load("assets/container.jpg", &width, &height, &nrChannels, 0);
+    unsigned char *data = stbi_load("assets/pirateBackground.jpg", &width, &height, &nrChannels, 0);
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
@@ -100,10 +100,10 @@ int main() {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-    data = stbi_load("assets/face.png", &width, &height, &nrChannels, 0);
+    data = stbi_load("assets/blood.png", &width, &height, &nrChannels, 0);
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
